044b6e49909f6d213957e20563a6e453f2a78963
bubb4rd <104111394+bubb4rd@users.noreply.github.com>
2026-06-10 13:32:51 -0500

    Initial Commit

---
+++ Wilmette Kenilworth PerksTests/Wilmette_Kenilworth_PerksTests.swift
@@ -0,0 +1,17 @@
+//
+//  Wilmette_Kenilworth_PerksTests.swift
+//  Wilmette Kenilworth PerksTests
+//
+//  Created by Bode Hubbard on 6/10/26.
+//
+
+import Testing
+@testable import Wilmette_Kenilworth_Perks
+
+struct Wilmette_Kenilworth_PerksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
